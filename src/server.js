8c227e21dde15e34160a1a6c1dddf51dec037f36
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2024-10-08 23:08:50 +0300

    server

---
+++ src/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+import { notFoundMiddleware } from './middlewares/notFound.js';
+import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
+import router from './routers/index.js';
+
+const PORT = Number(env(ENV_VARS.PORT, 3000));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    }),
+  );
+
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello',
+    });
+  });
+
+  app.use(router);
+
+  app.use(notFoundMiddleware);
+
+  app.use(errorHandlerMiddleware);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
